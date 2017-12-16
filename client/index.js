d92a7e178a93ca284af4a03b1382a3348f1ffeb4
Whitney Walker <whimsicalwhitney@gmail.com>
2017-12-16 12:20:39 -0700

    Receive, format, and send ajax request with form data

--- client/index.js
+++ client/index.js
@@ -1,3 +1,33 @@
 $(document).ready(() => {
-  alert('ready')
-})
+
+  var formatInputs = (data) => {
+    var formatted = {};
+    for(var i = 0; i < data.length-1; i++) {
+      formatted[data[i].id] = data[i].value;
+    }
+    return formatted;
+  }
+
+  var sendMessageReq = (data) => {
+    console.log(data)
+    $.ajax({
+      url: 'http://localhost:3000/',
+      contentType: 'application/json',
+      method: 'POST',
+      data: JSON.stringify(data)
+    })
+    .done((data) => {
+      console.log('success', data) 
+    })
+    .fail((err) => {
+      console.log('error', err);
+    });
+  }
+
+  $( "#form" ).submit(function(e) {
+    var messageObj = formatInputs($('#form :input'));
+    e.preventDefault();
+    sendMessageReq(messageObj);
+  });
+
+});
