ad847f9a869ee45dcafd701b93028a577b80bd03
Whitney Walker <whimsicalwhitney@gmail.com>
2017-12-15 20:32:18 -0700

    Set static files

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var app = express();
+console.log(__dirname)
+app.use(express.static(__dirname + '/../client/'));
 
-app.get('/', function(req, res){
-  res.send('hello world');
-});
+// app.get('/', function(req, res){
+//   res.send('hello world');
+// });
 
 app.listen(3000);
+
