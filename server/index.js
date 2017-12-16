35fa924aeb7e42e06757f1b2050ac0372726fa0e
Whitney Walker <whimsicalwhitney@gmail.com>
2017-12-16 12:20:07 -0700

    Use body parser

--- server/index.js
+++ server/index.js
@@ -5,6 +5,9 @@ const helpers = require('./helpers')
 
 app.use(express.static(__dirname + '/../client/'));
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.post('/', helpers.sendMessage, function(req, res){
   res.send('delivered');
 });
